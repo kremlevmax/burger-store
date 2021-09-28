b10720898999dabc936a38c0d32401bd48f06a6c
kremlevmax <kremlevmax1989@gmail.com>
2021-09-28 03:18:51 -0700

    Addint items to cart state in immutable way

--- src/store/CartStore/actions/addItemToCart.js
+++ src/store/CartStore/actions/addItemToCart.js
@@ -1,22 +1,47 @@
+import produce from "immer";
+
 export const addItemToCart = (state, action) => {
   const newItem = action.payload;
-  const existingItem = state.items.find((item) => item.id === newItem.id);
+  const existingItemIndex = state.items.findIndex((item) => item.id === newItem.id);
+  const totalPrice = state.totalPrice + newItem.price
+  const totalCalories = state.totalCalories + newItem.calories
+ // state.totalPrice += newItem.price;
+ // state.totalCalories += newItem.calories;
 
-  state.totalPrice += newItem.price;
-  state.totalCalories += newItem.calories;
-
-  if (!existingItem) {
-    state.items.push({
-      id: newItem.id,
-      calories: newItem.calories,
-      description: newItem.description,
-      image: newItem.image,
-      name: newItem.name,
-      price: newItem.price,
-      quantity: 1,
-    });
-  } else {
-    existingItem.quantity++;
+  if (existingItemIndex  < 0) {
+    const cart = {
+      ...state,
+      totalPrice: totalPrice,
+      totalCalories: totalCalories,
+      items: [...state.items, {
+        id: newItem.id,
+        calories: newItem.calories,
+        description: newItem.description,
+        image: newItem.image,
+        name: newItem.name,
+        price: newItem.price,
+        quantity: 1,
+      }
+    ]
   }
-  localStorage.setItem("cart", JSON.stringify(state));
-};
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  
+  return cart;
+    
+  } else {
+    const newItems = produce(state.items, draft => {draft[existingItemIndex].quantity += 1})
+    const cart = {
+      ...state, 
+      totalPrice: totalPrice,
+      totalCalories: totalCalories,
+      items: newItems
+      }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    return cart;
+  }
+}
+
+  // console.log( JSON.parse(JSON.stringify(state.items)))
