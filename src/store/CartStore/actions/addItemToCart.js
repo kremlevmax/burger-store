996e6324057857d49fa1dcba1a79f28125a48f30
kremlevmax <kremlevmax1989@gmail.com>
2021-09-28 18:49:22 -0700

    Immutable redux cart reducers state change via Immer

--- src/store/CartStore/actions/addItemToCart.js
+++ src/store/CartStore/actions/addItemToCart.js
@@ -2,18 +2,18 @@ import produce from "immer";
 
 export const addItemToCart = (state, action) => {
   const newItem = action.payload;
-  const existingItemIndex = state.items.findIndex((item) => item.id === newItem.id);
-  const totalPrice = state.totalPrice + newItem.price
-  const totalCalories = state.totalCalories + newItem.calories
- // state.totalPrice += newItem.price;
- // state.totalCalories += newItem.calories;
+  const existingItemIndex = state.items.findIndex(
+    (item) => item.id === newItem.id
+  );
 
-  if (existingItemIndex  < 0) {
-    const cart = {
-      ...state,
-      totalPrice: totalPrice,
-      totalCalories: totalCalories,
-      items: [...state.items, {
+  const totalPrice = state.totalPrice + newItem.price;
+  const totalCalories = state.totalCalories + newItem.calories;
+
+  if (existingItemIndex < 0) {
+    let cart = produce(state, (draft) => {
+      draft["totalCalories"] = totalCalories;
+      draft["totalPrice"] = totalPrice;
+      draft["items"].push({
         id: newItem.id,
         calories: newItem.calories,
         description: newItem.description,
@@ -21,27 +21,25 @@ export const addItemToCart = (state, action) => {
         name: newItem.name,
         price: newItem.price,
         quantity: 1,
-      }
-    ]
-  }
-
-  localStorage.setItem("cart", JSON.stringify(cart));
-  
-  return cart;
-    
-  } else {
-    const newItems = produce(state.items, draft => {draft[existingItemIndex].quantity += 1})
-    const cart = {
-      ...state, 
-      totalPrice: totalPrice,
-      totalCalories: totalCalories,
-      items: newItems
-      }
+      });
+    });
 
     localStorage.setItem("cart", JSON.stringify(cart));
+    return cart;
+  } else {
+    const newItems = produce(state.items, (draft) => {
+      draft[existingItemIndex].quantity += 1;
+    });
+    const cart = {
+      ...state,
+      totalPrice: totalPrice,
+      totalCalories: totalCalories,
+      items: newItems,
+    };
 
+    localStorage.setItem("cart", JSON.stringify(cart));
     return cart;
   }
-}
+};
 
-  // console.log( JSON.parse(JSON.stringify(state.items)))
+// console.log( JSON.parse(JSON.stringify(state.items)))
--- src/store/CartStore/actions/deleteItemFromCart.js
+++ src/store/CartStore/actions/deleteItemFromCart.js
@@ -1,14 +1,41 @@
-export const deleteItemFromCart = (state, action) => {
-  const itemToDelete = state.items.find(
-    (item) => item.id === action.payload.id
-  );
-  state.totalCalories -= itemToDelete.calories;
-  state.totalPrice -= itemToDelete.price;
+import produce from "immer";
 
-  if (itemToDelete.quantity === 1) {
-    state.items.filter((item) => item.id === itemToDelete.id);
-  } else {
-    itemToDelete.quantity--;
-  }
-  localStorage.setItem("cart", JSON.stringify(state));
+export const deleteItemFromCart = (state, action) => {
+  const itemToDelete = action.payload;
+
+  const cart = produce(state, (draft) => {
+    const index = draft.items.findIndex(
+      (item) => item.id === itemToDelete["id"]
+    );
+    if (draft.items[index].quantity > 1) {
+      draft.items[index].quantity -= 1;
+    } else {
+      draft.items.splice(index, 1);
+    }
+    console.log(itemToDelete.totalCalories);
+    draft["totalCalories"] -= itemToDelete.calories;
+    draft["totalPrice"] -= itemToDelete.price;
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  console.log(JSON.parse(JSON.stringify(cart)));
+  return cart;
+
+  // //state.totalCalories -= itemToDelete.calories;
+  // //state.totalPrice -= itemToDelete.price;
+
+  // if (state.items[itemToDeleteIndex].quantity === 1) {
+  //   const cart = produce(state.items, (draft) => {
+  //     draft.splice(itemToDeleteIndex, 1);
+  //   });
+
+  //   localStorage.setItem("cart", JSON.stringify(state));
+  //   return cart;
+  // } else {
+  //   const cart = produce(state.items, (draft) => {
+  //     draft[itemToDeleteIndex] -= 1;
+  //   });
+  //   localStorage.setItem("cart", JSON.stringify(state));
+  //   return cart;
+  // }
 };
