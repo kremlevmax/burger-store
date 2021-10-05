0fd22fd73b325f1f2c300a62d6bcafb8603827e7
kremlevmax <kremlevmax1989@gmail.com>
2021-10-05 16:35:16 -0700

    ESC button modal close

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import styles from "./Modal.module.css";
 
 const Modal = (props) => {
+  const closeOnEscapeButton = (event) => {
+    if ((event.charCode || event.keyCode) === 27) {
+      props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", closeOnEscapeButton);
+    return function cleanup() {
+      document.body.removeEventListener("keydown", closeOnEscapeButton);
+    };
+  });
+
   if (!props.show) {
     return null;
   }
+
   return (
     <div className={styles.modal} onClick={props.onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
