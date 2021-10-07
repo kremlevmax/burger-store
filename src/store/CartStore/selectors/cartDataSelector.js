bb70b3e37d62f211d747f6597cae1672fb071e34
kremlevmax <kremlevmax1989@gmail.com>
2021-10-07 11:09:30 -0700

    Single selector for total price and total calories

--- src/store/CartStore/selectors/cartDataSelector.js
+++ src/store/CartStore/selectors/cartDataSelector.js
@@ -1,30 +1,3 @@
 export const cartDataSelector = (state) => {
   return state.cart;
 };
-
-export const totalCaloriesSelector = (state) => {
-  let totalCalories = 0;
-  if (state.ingredients.ingredientsList.length > 0) {
-    state.cart.items.forEach((item) => {
-      const ingredient = state.ingredients.ingredientsList.find(
-        (ingredient) => ingredient.id === item.id
-      );
-      totalCalories = totalCalories + ingredient["calories"] * item.count;
-    });
-  }
-  return totalCalories;
-};
-
-export const totalPriceSelector = (state) => {
-  let totalPrice = 0;
-
-  if (state.ingredients.ingredientsList.length > 0) {
-    state.cart.items.forEach((item) => {
-      const ingredient = state.ingredients.ingredientsList.find(
-        (ingredient) => ingredient.id === item.id
-      );
-      totalPrice = totalPrice + ingredient["price"] * item.count;
-    });
-  }
-  return totalPrice;
-};
