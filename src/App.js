aaf0eb1c25b0dd036836932f5fbcd404028c990b
kremlevmax <kremlevmax1989@gmail.com>
2021-10-14 00:53:20 -0700

    Modal separated in two components. Gneneral Modal and child IngredientModal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { ingredientsListActions } from "./store/IngredientsStore/actions/Ingrefi
 
 import IngredientsList from "./components/IngredientsList/IngredientsList";
 import Cart from "./components/Cart/Cart";
+import IngredientModal from "./components/IngredientModal/IngredientModal";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,17 +21,19 @@ function App() {
   const ingredientForModal = useSelector(ingredientForModalSelector);
   const isModalShown = ingredientForModal.ingredientToShow !== undefined;
   const modalElement = isModalShown ? (
-    <Modal
-      show={isModalShown}
-      ingredient={ingredientForModal.ingredientToShow}
-      onClose={() => dispatch(ingredientsListActions.hideIngredientModal())}
-    />
+    <Modal show={isModalShown}>
+      <IngredientModal
+        ingredient={ingredientForModal.ingredientToShow}
+        onClose={() => dispatch(ingredientsListActions.hideIngredientModal())}
+      />
+    </Modal>
   ) : (
     <></>
   );
   return (
     <div className='App'>
-      {modalElement})<div className={styles.header}></div>
+      {modalElement}
+      <div className={styles.header}></div>
       <div className={styles.mainArea}>
         <div className={styles.ingredientsList}>
           <IngredientsList />
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,33 +1,12 @@
 import { useEffect } from "react";
 import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { cartActions } from "../../store/CartStore/actions/cartActions";
-import { cartDataSelector } from "../../store/CartStore/selectors/cartDataSelector";
 
 const Modal = (props) => {
-  const dispatch = useDispatch();
-  const cartData = useSelector(cartDataSelector);
-
-  const ingredientCount = cartData.items.find(
-    (item) => item.id === props.ingredient.id
-  )
-    ? cartData.items.find((item) => item.id === props.ingredient.id).count
-    : 0;
-
-  let buttonPhrase =
-    ingredientCount > 0
-      ? `You already have ${ingredientCount} pieces of this item in your cart`
-      : "Add to Cart";
-
   const closeOnEscapeButton = (event) => {
     if ((event.charCode || event.keyCode) === 27) {
       props.onClose();
     }
-  };
-
-  const addIngredientToCart = (item) => {
-    dispatch(cartActions.addItemToCart(item));
   };
 
   useEffect(() => {
@@ -43,31 +22,7 @@ const Modal = (props) => {
   const modal = (
     <div className={isModalShownClass} onClick={props.onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        <div className={styles.ingredientImageAndText}>
-          <div
-            className={styles.ingredientImage}
-            style={{ backgroundImage: `url(${props.ingredient.image})` }}
-          ></div>
-          <div className={styles.ingredientTextDescription}>
-            <div className={styles.ingredientName}>
-              {props.ingredient.name}
-              <div onClick={props.onClose} className={styles.closeXButton}>
-                X
-              </div>
-            </div>
-            <div>{props.ingredient.description}</div>
-            <div>Calories: {props.ingredient.calories}</div>
-            <div>Price: {props.ingredient.price}</div>
-          </div>
-        </div>
-        <div className={styles.addButtonArea}>
-          <button
-            className={styles.addButton}
-            onClick={() => addIngredientToCart(props.ingredient)}
-          >
-            {buttonPhrase}
-          </button>
-        </div>
+        {props.children}
       </div>
     </div>
   );
